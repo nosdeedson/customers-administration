194de97fa230f38005e5553d30b145e94439b6fa
nosdeedson <josemariazumira@gmail.com>
2022-10-16 08:46:54 -0300

    mudança versao

--- src/main/java/br/com/ejs/package-info.java
+++ src/main/java/br/com/ejs/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.2 
 // Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2022.06.15 às 08:32:47 PM BRT 
+// Gerado em: 2022.10.01 às 02:24:00 PM BRT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://ejs.com.br", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
